d1ee6e600ef4fbde6ac8272e66deb2222d12f0aa
Gkokkinos <george1red@gmail.com>
2017-11-19 19:37:03 +0100

    Save on SaveQ complete

--- Source/ATA_UU/SavedStatesQ.h
+++ Source/ATA_UU/SavedStatesQ.h
@@ -16,6 +16,18 @@ struct FDoubleArray
 	TArray<int32> ArrToAdd;
 };
 
+USTRUCT(BlueprintType)
+struct FStateAndActions
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Basic)
+	TArray<int32> actionArray;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Basic)
+	FString stateName;
+};
+
 /**
  * 
  */
@@ -39,6 +51,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Basic)
 	TArray<FDoubleArray> RewardMatrix;
 
+	//reward array 2
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Basic)
+	TArray<FStateAndActions> RewardArrayComplete;
 	//USavedStatesQ();
 	
 	
